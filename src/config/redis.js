93d79969d2819275859073700e48591a95c207dc
Shubham Kumar <shubhamk3304@gmail.com>
2026-09-22 16:38:15 +0530

    redis env setup

--- src/config/redis.js
+++ src/config/redis.js
@@ -6,7 +6,6 @@ import { createClient } from "redis";
 
 const redisClient = createClient({
   url: process.env.REDIS_URL,
-  token: process.env.REDIS_TOKEN,
 });
 
 redisClient.on("error", (error) => {
