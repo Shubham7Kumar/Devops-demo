69eca46e8b227cecc6f7ad99120cdb439272c74d
Shubham Kumar <shubhamk3304@gmail.com>
2026-09-23 08:40:08 +0530

    fix: correct api version

--- src/app.js
+++ src/app.js
@@ -54,7 +54,7 @@ app.get("/health",(req,res) => {
 app.get("/api-info",(req,res) => {
   res.json({
     name: "devops-demo-api",
-    version: "1.0.1",
+    version: "1.0.2",
     environment: NODE_ENV
   })
 })
